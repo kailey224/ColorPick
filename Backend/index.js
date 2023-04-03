e0f368646b39a6084a42a11e8ebcba1297adb1ca
kailey <kailey.lee224@gmail.com>
2023-04-03 20:58:41 +0900

    docs: edit

--- Backend/index.js
+++ Backend/index.js
@@ -13,15 +13,11 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true })) 
 
-app.post('/music-recommend', async function (req, res) {
-    let { myDateTime, userMessages, assistantMessages} = req.body
-
-    let todayDateTime = new Date().toLocaleString('ko-KR', { timeZone: 'Asia/Seoul' });
-
+app.post('/colorpick', async function (req, res) {
     let messages = [
-        {role: "system", content: "당신은 세계 최고의 점성술사입니다. 당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신의 이름은 챗도지입니다. 당신은 사람의 인생을 매우 명확하게 예측하고 운세에 대한 답을 줄 수 있습니다. 운세 관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다."},
-        {role: "user", content: "당신은 세계 최고의 점성술사입니다. 당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신의 이름은 챗도지입니다. 당신은 사람의 인생을 매우 명확하게 예측하고 운세에 대한 답을 줄 수 있습니다. 운세 관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다."},
-        {role: "assistant", content: "안녕하세요! 저는 챗도지입니다. 운세와 점성술에 관한 질문이 있으신가요? 어떤 것이든 물어보세요, 최선을 다해 답변해 드리겠습니다."},
+        {role: "system", content: "너는 키워드에 맞게 색상을 추천해주는 챗봇이야. 색상 추천은 5가지로 하고 컬러 영문 이름, 컬러코드, rgb를 JSON 형태로만 답해줘."},
+        {role: "user", content: "너는 키워드에 맞게 색상을 추천해주는 챗봇이야. 색상 추천은 5가지로 하고 컬러 영문 이름, 컬러코드, rgb를 JSON 형태로만 답해줘."},
+        {role: "assistant", content: "원하시는 키워드를 입력해주세요."},
     ]
 
     while (userMessages.length != 0 || assistantMessages.length != 0) {
@@ -41,6 +37,7 @@ app.post('/music-recommend', async function (req, res) {
         model: "gpt-3.5-turbo",
         messages: messages
     });
+    
     let fortune = completion.data.choices[0].message['content']
 
     res.json({"assistant": fortune});
--- Frontend/script.js
+++ Frontend/script.js
@@ -1,36 +1,31 @@
 const chatBox = document.querySelector('.chat-box');
 let userMessages = [];
 let assistantMessages = [];
-let myDateTime = ''
 
 const sendMessage = async () => {
     const chatInput = document.querySelector('.chat-input input');
     const chatMessage = document.createElement('div');
+    
     chatMessage.classList.add('chat-message');
-    chatMessage.innerHTML = `
-<p>${chatInput.value}</p>
-`;
+    chatMessage.innerHTML = `<p>${chatInput.value}</p>`;
     chatBox.appendChild(chatMessage);
     
     //userMessage 메세지 추가
     userMessages.push(chatInput.value);
-
     chatInput.value = '';
 
-    const response = await fetch('http://localhost:3000//music-recommend', {
+    const response = await fetch('http://localhost:3000/colorpick', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            myDateTime: myDateTime,
             userMessages: userMessages,
             assistantMessages: assistantMessages,
         })
     });
 
     const data = await response.json();
-    document.getElementById('loader').style.display = "none";
     
     //assistantMessage 메세지 추가
     assistantMessages.push(data.assistant);
@@ -38,8 +33,8 @@ const sendMessage = async () => {
     const astrologerMessage = document.createElement('div');
     astrologerMessage.classList.add('chat-message');
     astrologerMessage.innerHTML = `
-<p class='assistant'>${data.assistant}</p>
-`;
+        <p class='assistant'>${data.assistant}</p>
+    `;
     chatBox.appendChild(astrologerMessage);
 };
 
